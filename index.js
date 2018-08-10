f3e4bfb223b30bebadd70d4b2b867e8f862f36b9
XReid <xmeister86@gmail.com>
2018-08-09 23:08:36 -0700

    change server port

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 app.use(authRoutes);
 
-app.listen(8000, process.env.IP, function () {
+app.listen(process.env.PORT, process.env.IP, function () {
     console.log("Yelp Camp server is now starting...");
 });
